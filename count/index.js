9e0fc3736f85b28131b6650f02df9a43175d7f7a
Rayhan Alam Towhid <rayhan.cse9007@baiust.ac.bd>
2025-07-18 17:00:39 +0600

    index.js

--- count/index.js
+++ count/index.js
@@ -2,17 +2,22 @@ const decreseBtn = document.getElementById("decreseBtn");
 const resetBtn = document.getElementById("resetBtn");
 const increseBtn = document.getElementById("increseBtn");
 const countLabel = document.getElementById("countLabel");
-let count = 0;
 
-increseBtn.onclick = function(){
-    count++;
+let count = Number(localStorage.getItem('count')) || 0;
+countLabel.textContent = count;
+
+function updateAndSaveCount(newValue) {
+    count = newValue;
     countLabel.textContent = count;
+    localStorage.setItem('count', count);
 }
-decreseBtn.onclick = function(){
-    count--;
-    countLabel.textContent = count;
+
+increseBtn.onclick = function() {
+    updateAndSaveCount(count + 1);
 }
-resetBtn.onclick = function(){
-    count = 0;
-    countLabel.textContent = count;
+decreseBtn.onclick = function() {
+    updateAndSaveCount(count - 1);
+}
+resetBtn.onclick = function() {
+    updateAndSaveCount(0);
 }
